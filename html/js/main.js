e82309f9481bf9f48d244fd9184d0fd90fe3eae5
ppakunn <me@albertlua.com>
2021-09-18 02:09:06 -0500

    Initial UI

---
+++ html/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+    // $('#searchTxt').hide();
+
+    // $('#searchBtn').on({
+    //     mouseenter: function() {
+    //         $('#searchTxt').fadeIn();
+    //         $('#addBtn').fadeOut();
+    //     }, mouseleave: function() {
+    //         $('#searchTxt').fadeOut();
+    //         $('#addBtn').fadeIn();
+    //     }, click: function() {
+    //         console.log('whoo');
+    //     }
+    // });
+});
