3718e8d83e0d827d6da047e76adc348fbe315287
ppakunn <me@albertlua.com>
2021-09-18 13:55:10 -0500

    Update range selector

--- html/js/main.js
+++ html/js/main.js
@@ -28,14 +28,40 @@ $(function() {
         }
     });
 
-    // Fetch JSON
-    $.getJSON('test.json', function(data) {
-        var item = '';
-
-        // Go through JSON
-        $.each(data.items, function(key, value) {
-            item += '<tr><td>'+value.correlation+'</td><td>'+value.frequency+'</td><td>'+value.productID+'</td>';
-        })
-        $('#dataResult').append(item)
+    // Rating range update
+    $('#rating').on('input', function() {
+        val = 
+        $('#ratingNum').html($('#rating').val());
     })
+
+    // Fetch JSON
+    $('#searchBtn').on({
+        click: function() {
+            $.getJSON('test.json', function(data) {
+                var item = '';
+        
+                // Go through JSON
+                $.each(data.items, function(key, value) {
+                    item += '<tr><td>'+value.correlation+'</td><td>'+value.frequency+'</td><td>'+value.productID+'</td>';
+                })
+                $('#dataResult').append(item)
+            })
+        }
+    })
+    
+    /*
+    $('#guess').on('input', function() {
+        if ($('#guess').val()) {
+            //prevents empty query to sqlite
+            $.get("/autocomplete/" + $('#guess').val(), function(data) { //sends GET request to server returning {'data':[...]}
+                var results = "";
+                data['data'].forEach((result) => { results += <div class="result">
+            <p>${result}</p></div>; }); //inserts div for each matching anime from database
+                $('.search-results').html(results); //updates html
+                $('.search-results').show(); //shows if it is hidden
+            });
+        } else {
+            $('.search-results').hide(); //if empty query, hide search results just in case
+        }
+    }); */
 });
